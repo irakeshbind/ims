cf709c2519879cb5e5593a9855d34c8941bec0ed
irakeshbind <rakeshbind351998@gmail.com>
2025-02-17 10:22:23 -0600

    add image

--- sbs-school/src/components/Signup.js
+++ sbs-school/src/components/Signup.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
-const Signup=()=>{
-    return(
-<h1>Signup</h1>
+const Signup = () => {
+    return (
+ <div className='signup-wrapper'>
+  <div className='signup-box'>
+    <div className='signup-left'>
+     <img src={require('./assets/img2.png')}/>
+      <h2 className='signup-heading'>sbs online classes</h2>
+      <p className='signup-para'>Learn Coding in easy  way....</p>
+    </div>
+    <div className='signup-right'>
+
+    </div>
+  </div>
+ </div>
     )
 }
 export default Signup
