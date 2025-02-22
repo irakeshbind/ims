9c55208a76a961a12c455e194f2ca1d2dc8ee6ed
irakeshbind <rakeshbind351998@gmail.com>
2025-02-22 04:30:43 -0600

    dashboard subroute create

--- sbs-school/src/App.js
+++ sbs-school/src/App.js
@@ -3,15 +3,34 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Dashboard from './components/Dashboard'
 import Login from './components/Login'
 import Signup from './components/Signup'
+
+import Home from './components/Home'
+import Courses from './components/Courses'
+import AddCourses from './components/AddCourses'
+import Students from './components/Student'
+import AddStudent from './components/AddStudent'
+import CollectFee from './components/CollectFee'
+import PaymentHistory from './components/PaymentHistory'
+
+
 import './components/style.css'
 import { ToastContainer } from 'react-toastify';
 
 
 const App=()=>{
   const myRouter = createBrowserRouter([
+    {path:'',Component:Login},
     {path:'login',Component:Login},
     {path:'signup',Component:Signup},
-    {path:'dashboard',Component:Dashboard}
+    {path:'dashboard',Component:Dashboard,children:[
+      {path:'',Component:Home},
+      {path:'courses',Component:Courses},
+      {path:'add-course',Component:AddCourses},
+      {path:'students',Component:Students},
+      {path:'add-student',Component:AddStudent},
+      {path:'collect-fee',Component:CollectFee},
+      {path:'payment-history',Component:PaymentHistory},
+    ]}
     
   ])
     return(
---
+++ sbs-school/src/components/AddStudent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const AddStudent = () => {
+    return (
+        <h1>AddStudent</h1>
+    )
+}
+export default AddStudent
