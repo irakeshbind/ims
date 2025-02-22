c15d5c2a9f8c2b6c2671b003ce81fabe3e527e3a
irakeshbind <rakeshbind351998@gmail.com>
2025-02-22 02:02:35 -0600

    dashbord is complete

--- sbs-school/src/components/SideNav.js
+++ sbs-school/src/components/SideNav.js
@@ -11,11 +11,19 @@ const SideNav = () => {
                 </div>
             </div>
             <div className='menu-container'>
-                <Link className='menu-link'>Add Students</Link>
-                <Link className='menu-link' >Home</Link>
-                <Link className='menu-link' >All Students</Link>
-                <Link className='menu-link' >Payment Historys</Link>
-                <Link className='menu-link' >AllCourse</Link>
+            <Link className='menu-link' ><i className="fa fa-home" aria-hidden="true"></i> Home</Link>
+            <Link className='menu-link' ><i class="fa fa-book" aria-hidden="true"></i>All Courses</Link>
+                <Link className='menu-link'><i class="fa fa-plus" aria-hidden="true"></i>Add Courses</Link>
+                <Link className='menu-link' ><i class="fa fa-users" aria-hidden="true"></i>All Students</Link>
+                <Link className='menu-link' ><i class="fa fa-plus" aria-hidden="true"></i>Add Students</Link>
+                
+                <Link className='menu-link' ><i class="fa fa-credit-card" aria-hidden="true"></i>Collect Fee</Link>
+                <Link className='menu-link' ><i class="fa fa-list" aria-hidden="true"></i>Payment History</Link>
+
+            </div>
+            <div className='contact-us'>
+                <p><i class="fa fa-address-book" aria-hidden="true"></i>Contact Developer</p>
+                <p><i class="fa fa-phone" aria-hidden="true"></i>9326827804</p>
             </div>
         </div>
 
